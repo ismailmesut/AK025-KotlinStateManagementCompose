1cbdce7ef062fa453105b3fcecb5b2ae08e06183
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-07 19:41:12 +0300

    updated: other composables

--- app/src/main/java/com/ismailmesutmujde/kotlinstatemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstatemanagementcompose/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ismailmesutmujde.kotlinstatemanagementcompose.ui.theme.KotlinStateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +27,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KotlinStateManagementComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Surface(color = Color.LightGray) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hello",
+                fontSize = 26.sp
+                )
+            Spacer(modifier = Modifier.padding(7.dp))
+            Button(onClick = {
+                println("clicked")
+            }) {
+                Text(text = "Button")
+                Text(text = "Test")
+            }
+        }
+    }
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KotlinStateManagementComposeTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
