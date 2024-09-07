9ea0989459356bec433cf7c6949fbf7abdf961bc
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-07 19:30:26 +0300

    initial commit

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinstatemanagementcompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ismailmesutmujde.kotlinstatemanagementcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ismailmesutmujde.kotlinstatemanagementcompose.ui.theme.KotlinStateManagementComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinStateManagementComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinStateManagementComposeTheme {
+        Greeting("Android")
+    }
+}
